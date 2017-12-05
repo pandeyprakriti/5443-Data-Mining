65af232bcf5ff28b3be00f1484c6579f64848f8e
Prakriti Pandey <prakretee@gmail.com>
2017-12-05 12:15:09 -0600

    Update run.py

--- Assignments/scrape_data/run.py
+++ Assignments/scrape_data/run.py
@@ -1,47 +1,45 @@
-# -*- coding: utf-8 -*-
 """
-Spyder Editor
-This is a temporary script file.
+Created on Wed Nov 29 12:30:39 2017
+
+@author: prakritipandey
+This program will get the name and firm name of lawyers from the website link given below in json format"
 """
 
+import sys
 import requests
+import bs4
 from bs4 import BeautifulSoup
 import json
-company_details=[]
-url=requests.get("https://www.yellowpages.com/search?search_terms=computer&geo_location_terms=Los+Angeles%2C+CA")
-#To get the data from web page  
-#print(r.content)//to print the content
-soup=BeautifulSoup(url.content,"lxml")
-#To convert the content in some readable form
-#print (soup.prettify())
-#To cleanup code and make easier to read
 
-data= soup.find_all("div",{"class":"info"})
-#To find the content from class info
-#print(data)
 
-for item in data:
-         name=(item.contents[0].find_all("a",{"class":"business-name"})[0].text)
-        #To print the content from class business name
-         
-         try:
-          postal=(item.contents[1].find_all("span",{"itemprop":"postalCode"})[0].text)
-         except:
-            pass
-         try:
-          phone=(item.contents[1].find_all("li",{"class":"primary"})[0].text)
-         except:
-            pass
-        
-        
-        
-         company={}
-         company['name']=name
-         company['postal']=postal
-         company['phone']=phone
-         company_details.append(company)
-#print(emp_details)
-with open('output.json', 'w') as f:                             # write data into json file
-    output=json.dumps(company_details,indent=1)
-    f.write(output)     
-print(company_details)
+
+details = []
+#website link
+url ="https://www.superlawyers.com/texas/toplists/top-100-2017-texas-super-lawyers/96a83c96abbe6d0b40c72b279ebdf76e"            # the websit
+#print(url)
+
+# Beautiful soup way
+html = requests.get(url).text
+soup = BeautifulSoup(html,"html.parser")
+data = soup.find_all('div',{'class':"top_list floating_lawyers container"})
+
+for lawyer in data:
+    
+    
+    # find name of lawyer
+    names  = lawyer.findNext('p',{'class':"floating_lawyers full_name"})         
+    names = names.findNext('a')
+    name=names.text
+    #find the firm name 
+    firmnames=lawyer.findNext('p',{'class':"floating_lawyers firm_name"})
+    firmname=firmnames.text
+    lawyer_dict = {}                                            
+    lawyer_dict['Name'] = name.strip()
+    lawyer_dict['Address'] = firmname.strip()
+    #append to list
+    details.append(lawyer_dict)       
+# to save the output file in json format
+with open('output.json', 'w+') as f:                             
+    output = json.dumps(details,indent=1)     
+    f.write(output)
+print(details)
